d256d505971659551d1c44566e97a940ac4cf7a9
Zac Stewart <zgstewart@gmail.com>
2016-03-16 13:52:09 -0400

    Use timers to schedule message relays

--- lib/comm/message_relay.rb
+++ lib/comm/message_relay.rb
@@ -5,50 +5,43 @@ module Comm
     include Celluloid::IO
     include Celluloid::Internals::Logger
 
-    class Scheduler
-      attr_reader :count, :relay_at
-
-      def initialize
-        @count = 0
-        @relay_at = Time.now.to_i
-      end
-
-      def increment
-        @count += 1
-        @relay_at = Time.now.to_i + delay
-      end
-
-      private
-
-      def delay
-        @count ** 2 - 1
-      end
-    end
-
     def initialize(node)
       @node = node
-      @messages = Hash.new
+      @messages = Hash.new(0)
+      @timer_group = Timers::Group.new
+      @timers = {}
     end
 
     def add(message)
-      @messages.fetch(message) do
-        puts "-> Adding message to relay"
-        @messages[message] = Scheduler.new
+      return if relaying?(message)
+
+      info "-> Adding message to relay"
+      async.relay_message(message)
+    end
+
+    def relay_message(message)
+      @timers[message] = @timer_group.after(delay_for(message)) do
+        info "-> Relaying message"
+        @timers.delete(message)
+        @messages[message] += 1
+        @node.broadcast(message)
+        async.add(message)
       end
     end
 
     def run
-      message, scheduler = @messages.select do |_, s|
-        s.relay_at <= Time.now.to_i
-      end.max_by { |_, s| s.relay_at }
-
-      if message && scheduler
-        info "-> Relay #{message.inspect}, #{scheduler.inspect}"
-        @node.broadcast(message)
-        scheduler.increment
-      end
-
+      @timer_group.wait
       async.run
+    end
+
+    private
+
+    def delay_for(message)
+      @messages[message] ** 2 - 1
+    end
+
+    def relaying?(message)
+      @timers.has_key?(message)
     end
   end
 end
