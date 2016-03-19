2667b0a24f50b711ab15a61ad8bea9f870665027
Zac Stewart <zgstewart@gmail.com>
2016-03-19 08:03:29 -0400

    Use a set instead of a hash to back peer pool

--- lib/comm/peer_pool.rb
+++ lib/comm/peer_pool.rb
@@ -1,27 +1,28 @@
+require 'set'
 module Comm
   class PeerPool
     def initialize(size: 2)
-      @peers = {}
+      @peers = Set.new
       @size = size
     end
 
     def add(peer, &on_add)
-      return if @peers.has_key?(peer.address)
-      return if @peers.size >= @size
-
-      puts "-> Adding peer #{peer.inspect}"
-      @peers[peer.address] = peer
-      yield peer
+      if @peers.add?(peer)
+        if @peers.include?(peer)
+          puts "-> Added peer #{peer.inspect}"
+          yield peer
+        end
+      end
     end
 
     def each(&block)
       return enum_for(__method__) unless block_given?
 
-      @peers.values.each(&block)
+      @peers.each(&block)
     end
 
     def remove(peer)
-      @peers.delete(peer.address)
+      @peers.delete(peer)
     end
   end
 end
