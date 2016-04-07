0bbd9e3fbc2abde9c931dba45190d986fadc874f
Zac Stewart <zgstewart@gmail.com>
2016-04-07 10:40:59 -0400

    Start basic implementation of Address

---
+++ README.md
@@ -0,0 +1,22 @@
+# Comm
+
+A hypothetical, distributed, encrypted, instant messaging protocol.
+
+## Requirements
+
+* No centralized server
+* Messages should be stored in the network for some reasonable amount of time until recipient is available
+
+## What the what
+
+0. Client announces a message to the network
+  - message has a unique id
+  - message has a recipient address (public key)
+  - message has a "message" that only recipient can read (using their private key)
+0. Network nodes store and re-announce messages they receive
+0. Network nodes drop messages that they've re-announced for a reasonable amount of time, and a reasonable number of times
+0. Recipient finally gets message, announces acknowledgement
+  - acknowledgement has message's unique id
+  - acknowledgement has signature of message recipient (using their private key)
+0. Network nodes drop messages after verifying their acknowledgement
+0. Network nodes re-announce acknowledgement a reasonable number of times
---
+++ src/address.rs
@@ -0,0 +1,53 @@
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+use std::iter::repeat;
+
+struct Address {
+    data: [u8; 20]
+}
+
+impl Address {
+    pub fn for_content(content: &str) -> Address {
+        let mut hasher = Sha1::new();
+        hasher.input_str(content);
+        let mut data = [0; 20];
+        hasher.result(&mut data);
+        Address {
+            data: data
+        }
+    }
+
+    pub fn from_str(string: &str) -> Address {
+        use rustc_serialize::hex::FromHex;
+        let bytes = string.from_hex().unwrap();
+        let mut data = [0u8; 20];
+        for (place, byte) in data.iter_mut().zip(bytes.iter()) {
+            *place = *byte;
+        }
+        Address {
+            data: data
+        }
+    }
+
+    pub fn to_str(&self) -> String {
+        use rustc_serialize::hex::ToHex;
+        self.data.to_hex()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Address;
+
+    #[test]
+    fn test_for_content() {
+        let address = Address::for_content("some string");
+        assert_eq!(address.to_str(), "8b45e4bd1c6acb88bebf6407d16205f567e62a3e");
+    }
+
+    #[test]
+    fn test_from_str() {
+        let address = Address::from_str("8b45e4bd1c6acb88bebf6407d16205f567e62a3e");
+        assert_eq!(address.to_str(), "8b45e4bd1c6acb88bebf6407d16205f567e62a3e");
+    }
+}
