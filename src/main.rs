6a28e4354725c04d67e97a3fc1379757b70f1ac6
Zac Stewart <zgstewart@gmail.com>
2016-04-20 07:31:06 -0400

    Move simple bootstrapping logic into network module

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate rustc_serialize;
 extern crate time;
 
 use address::Address;
-use node::{Node, UdpNode};
+use node::UdpNode;
 use server::Server;
 use std::env;
 use std::sync::{Arc, Mutex, mpsc};
@@ -28,27 +28,14 @@ fn main() {
     let (tx, incoming) = mpsc::channel();
     server::UdpServer::new(port).start(tx);
 
-    let mut my_origin = messages::Node::new();
-    my_origin.set_id(address.to_str());
-    my_origin.set_ip_address(vec![127, 0, 0, 1]);
-    my_origin.set_port(port as u32);
-
+    let self_node = node::UdpNode::new(address, ("127.0.0.1", port));
     let routing_table = routing_table::RoutingTable::new(8, address);
+    let bootstrap_address = ("127.0.0.1", args[3].clone().parse::<u16>().unwrap());
+    let bootstrap_node = UdpNode::new(Address::null(), bootstrap_address);
 
-    if let Some(bootstrap) = args.get(3) {
-        println!("Bootstrapping");
-        let bootstrap_address = ("127.0.0.1", bootstrap.clone().parse::<u16>().unwrap());
-        let mut find_node_query = messages::FindNodeQuery::new();
-        find_node_query.set_origin(my_origin.clone());
-        find_node_query.set_target(address.to_str());
-        let mut envelope = messages::Envelope::new();
-        envelope.set_message_type(messages::Envelope_Type::FIND_NODE_QUERY);
-        envelope.set_find_node_query(find_node_query);
-        let node = UdpNode::new(Address::null(), bootstrap_address);
-        node.send(envelope);
-    }
+    let handler = network::Handler::new(Arc::new(Mutex::new(routing_table)), self_node);
 
-    let handler = network::Handler::new(Arc::new(Mutex::new(routing_table)));
-    handler.run(my_origin.clone(), incoming);
+    handler.run(incoming, bootstrap_node);
+
     loop { }
 }
--- src/network.rs
+++ src/network.rs
@@ -1,65 +1,92 @@
-use protobuf;
-use messages;
-use node;
-use node::{Node, Deserialize, Serialize};
 use address::Address;
+use messages;
+use node::{Node, Deserialize, Serialize};
+use node;
+use protobuf;
 use routing_table::RoutingTable;
 use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
+use std::collections::HashSet;
 
 pub struct Handler {
-    routing_table: Arc<Mutex<RoutingTable>>
+    routing_table: Arc<Mutex<RoutingTable>>,
+    self_node: Arc<Box<node::Node + 'static>>,
 }
 
 impl Handler {
-    pub fn new(routing_table: Arc<Mutex<RoutingTable>>) -> Handler {
-        Handler { routing_table: routing_table }
+    pub fn new<N: node::Node + 'static>(
+        routing_table: Arc<Mutex<RoutingTable>>,
+        self_node: N) -> Handler {
+
+        Handler {
+            routing_table: routing_table,
+            self_node: Arc::new(Box::new(self_node)),
     }
 
-    pub fn run(self, my_origin: messages::Node, messages: mpsc::Receiver<(usize, [u8; 4096])>) {
+    pub fn run<N: node::Node + 'static>(
+        mut self,
+        messages: mpsc::Receiver<(usize, [u8; 4096])>,
+        bootstrap_node: N) {
+
+        let bootstrap_node = Box::new(bootstrap_node);
+
         thread::spawn(move || {
+            self.query_node_for_self(&bootstrap_node);
             loop {
                 let (size, buf) = messages.recv().unwrap();
                 let message = protobuf::parse_from_bytes::<messages::Envelope>(&buf[0..size]).unwrap();
-                let mut routing_table = self.routing_table.lock().unwrap();
-                println!("Message: {:?}", message);
-
-                match message.get_message_type() {
-                    messages::Envelope_Type::FIND_NODE_QUERY => {
-                        let find_node_query = message.get_find_node_query();
-                        let origin = node::UdpNode::deserialize(find_node_query.get_origin());
-                        let target = Address::from_str(find_node_query.get_target());
-
-                        {
-                            let nodes: Vec<&Box<node::Node>> = routing_table.nearest_to(&target);
-                            println!("Nearest nodes: {:?}", nodes);
-                            let nodes: Vec<messages::Node> = nodes.iter().map(|n| n.serialize()).collect();
-                            let mut find_node_response = messages::FindNodeResponse::new();
-                            find_node_response.set_origin(my_origin.clone());
-                            find_node_response.set_nodes(
-                                protobuf::RepeatedField::from_slice(nodes.as_slice())
-                                );
-                            let mut response = messages::Envelope::new();
-                            response.set_message_type(messages::Envelope_Type::FIND_NODE_RESPONSE);
-                            response.set_find_node_response(find_node_response);
-
-                            origin.send(response);
-                        }
-
-                        routing_table.insert(origin);
-                    }
-                    messages::Envelope_Type::FIND_NODE_RESPONSE => {
-                        let find_node_response = message.get_find_node_response();
-                        let origin = node::UdpNode::deserialize(find_node_response.get_origin());
-                        routing_table.insert(origin);
-
-                        for node in find_node_response.get_nodes() {
-                            let node = node::UdpNode::deserialize(node);
-                            routing_table.insert(node);
-                        }
-                    }
-                }
+                self.handle_message(message);
             }
         });
+    }
+
+    fn handle_message(&mut self, message: messages::Envelope) {
+        println!("Message: {:?}", message);
+
+        match message.get_message_type() {
+            messages::Envelope_Type::FIND_NODE_QUERY => {
+                let mut routing_table = self.routing_table.lock().unwrap();
+                let find_node_query = message.get_find_node_query();
+                let origin = node::UdpNode::deserialize(find_node_query.get_origin());
+                let target = Address::from_str(find_node_query.get_target());
+
+                {
+                    let nodes: Vec<&Box<node::Node>> = routing_table.nearest_to(&target);
+                    println!("Nearest nodes: {:?}", nodes);
+                    let nodes: Vec<messages::Node> = nodes.iter().map(|n| n.serialize()).collect();
+                    let mut find_node_response = messages::FindNodeResponse::new();
+                    find_node_response.set_origin(self.self_node.serialize());
+                    find_node_response.set_nodes(protobuf::RepeatedField::from_slice(nodes.as_slice()));
+                    let mut response = messages::Envelope::new();
+                    response.set_message_type(messages::Envelope_Type::FIND_NODE_RESPONSE);
+                    response.set_find_node_response(find_node_response);
+
+                    origin.send(response);
+                }
+
+                routing_table.insert(origin);
+            }
+            messages::Envelope_Type::FIND_NODE_RESPONSE => {
+                let mut routing_table = self.routing_table.lock().unwrap();
+
+                let find_node_response = message.get_find_node_response();
+                let origin = node::UdpNode::deserialize(find_node_response.get_origin());
+                routing_table.insert(origin);
+
+                for node in find_node_response.get_nodes() {
+                    let node = node::UdpNode::deserialize(node);
+                    routing_table.insert(node);
+                }
+            }
+        }
+    }
+    fn query_node_for_self<N: node::Node + 'static>(&mut self, node: &Box<N>) {
+        let mut find_node_query = messages::FindNodeQuery::new();
+        find_node_query.set_origin(self.self_node.serialize());
+        find_node_query.set_target(self.self_node.get_address().to_str());
+        let mut envelope = messages::Envelope::new();
+        envelope.set_message_type(messages::Envelope_Type::FIND_NODE_QUERY);
+        envelope.set_find_node_query(find_node_query);
+        node.send(envelope);
     }
 }
