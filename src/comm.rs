d0fd7c157ac88c6932d08549a9e4d1665ef74563
Zac Stewart <zgstewart@gmail.com>
2016-07-23 16:40:54 -0400

    Build a library in addition to a binary

---
+++ src/comm.rs
@@ -0,0 +1,21 @@
+extern crate env_logger;
+extern crate crypto;
+#[macro_use]
+extern crate log;
+extern crate mio;
+extern crate num;
+extern crate protobuf;
+extern crate rand;
+extern crate rustc_serialize;
+extern crate time;
+
+pub mod address;
+pub mod client;
+pub mod messages;
+pub mod network;
+pub mod node;
+pub mod node_bucket;
+pub mod routing_table;
+pub mod transaction;
+#[cfg(test)]
+mod tests;
