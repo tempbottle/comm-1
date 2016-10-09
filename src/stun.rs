d768d46e48b3fce8d52ad3692101ccd5839453f3
Zac Stewart <zgstewart@gmail.com>
2016-10-09 15:02:08 -0400

    Use stun 0.0.2

--- src/stun.rs
+++ src/stun.rs
@@ -4,7 +4,11 @@ use std::net::SocketAddr;
 
 pub fn get_mapped_address(local_addr: SocketAddr) -> Result<SocketAddr, String> {
     let local_port = local_addr.port();
-    let client = stun::Client::new("stun.l.google.com:19302", local_port);
+    let ip_version = match local_addr {
+        SocketAddr::V4(_) => stun::IpVersion::V4,
+        SocketAddr::V6(_) => stun::IpVersion::V6
+    };
+    let client = stun::Client::new("stun.l.google.com:19302", local_port, ip_version);
     let mesage = stun::Message::request();
     let response = client.send(mesage.encode());
     let stun::Message { attributes, ..} = stun::Message::decode(response);
