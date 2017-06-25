bbf29f34b9f8dcb6e441102f5af6d8f3b9584e8d
Zac Stewart <zgstewart@gmail.com>
2017-06-25 18:04:25 -0400

    Add rampup arg to multi CLI
    This let's me specify a time to wait in between starting nodes

--- src/multi.rs
+++ src/multi.rs
@@ -12,7 +12,7 @@ use comm::client;
 use comm::network;
 use comm::node;
 
-pub fn start_multiple(host: &str, port_start: u16, port_end: u16, router_host: Option<&str>) {
+pub fn start_multiple(host: &str, port_start: u16, port_end: u16, router_host: Option<&str>, rampup: u64) {
     let min = 0.to_biguint().unwrap();
     let max = num::pow(2.to_biguint().unwrap(), LENGTH);
 
@@ -34,7 +34,7 @@ pub fn start_multiple(host: &str, port_start: u16, port_end: u16, router_host: O
         let network = network::Network::new(address, socket_address, routers);
         let client = client::Client::new(address);
         client.run(network);
-        thread::sleep(std::time::Duration::from_millis(100));
+        thread::sleep(std::time::Duration::from_millis(rampup));
     }
 
     info!("All running :)");
@@ -48,7 +48,8 @@ fn main() {
     let host = args[1].as_str();
     let port_start = args[2].clone().parse::<u16>().unwrap();
     let port_end = args[3].clone().parse::<u16>().unwrap();
-    let router = args.get(4).map(|h| h.as_str());
+    let rampup = args[4].clone().parse::<u64>().unwrap();
+    let router = args.get(5).map(|h| h.as_str());
 
-    start_multiple(host, port_start, port_end, router);
+    start_multiple(host, port_start, port_end, router, rampup);
 }
