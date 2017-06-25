638d6b7fa9c1a64f8f44f881af5bf757e0aacc7c
Zac Stewart <zgstewart@gmail.com>
2017-06-25 17:04:20 -0400

    Add debug logging for Bucket insert & split

--- src/node_bucket.rs
+++ src/node_bucket.rs
@@ -73,6 +73,8 @@ impl NodeBucket {
     }
 
     pub fn insert(&mut self, node: Box<Node>) -> InsertionResult {
+        debug!("Inserting node {:?} into bucket {:?}", &node, self);
+
         let address = node.address();
         if self.covers(&address) {
             if let Some(pos) = self.addresses.iter().position(|a| a == &address) {
@@ -136,6 +138,8 @@ impl NodeBucket {
     }
 
     pub fn split(self) -> (Self, Self) {
+        debug!("Splitting bucket {:?}", &self);
+
         let difference = &self.max - &self.min;
         let partition = difference / 2.to_biguint().unwrap() + &self.min;
         let (a_addresses, b_addresses) = self.addresses
