54d0d327dd41e6bc5a9ce73a3921562d98fa6d23
Zac Stewart <zgstewart@gmail.com>
2018-02-11 11:18:16 -0500

    Randomly use a STUN server from a static set
    These should be configurable eventually

--- src/stun.rs
+++ src/stun.rs
@@ -1,14 +1,26 @@
 extern crate stun;
+extern crate rand;
 
+use rand::Rng;
 use std::net::SocketAddr;
 
+// TODO: These should be part of configuration
+const STUN_SERVERS: &'static [&'static str] = &[
+    "stun.l.google.com:19302",
+    "stun1.l.google.com:19302",
+    "stun2.l.google.com:19302",
+    "stun3.l.google.com:19302",
+    "stun4.l.google.com:19302"
+];
+
 pub fn get_mapped_address(local_addr: SocketAddr) -> Result<SocketAddr, String> {
+    let stun_server = rand::thread_rng().choose(STUN_SERVERS).expect("No STUN server");
     let local_port = local_addr.port();
     let ip_version = match local_addr {
         SocketAddr::V4(_) => stun::IpVersion::V4,
         SocketAddr::V6(_) => stun::IpVersion::V6
     };
-    let client = stun::Client::new("stun.l.google.com:19302", local_port, ip_version);
+    let client = stun::Client::new(stun_server, local_port, ip_version);
     let mesage = stun::Message::request();
     let response = client.send(mesage.encode());
     let stun::Message { attributes, ..} = stun::Message::decode(response);
